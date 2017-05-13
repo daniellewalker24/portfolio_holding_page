eca1f31327e4db26675b2b4262b9b2dc5cb92fa0
Danielle <daniellewalker24@users.noreply.github.com>
2017-05-13 15:32:44 +0100

    implementing slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require vendor/slider
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'application#index'
+    get 'slider', to: 'application#slider'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
